e7d65fae17d7810b4496a362d0e7f25d100edf67
Pratiksha0401 <pratiksha411998@gmail.com>
2025-02-02 12:12:32 +0530

    invokeall method

--- src/main/java/org/example/executorService/SingleThreadFuture.java
+++ src/main/java/org/example/executorService/SingleThreadFuture.java
@@ -1,10 +1,19 @@
 package org.example.executorService;
 
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class SingleThreadFuture {
+    /**
+     *
+     * @param args
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         Future<Integer>  future = executorService.submit(() -> 42);
         if(future.isDone()){
@@ -33,14 +42,38 @@ public class SingleThreadFuture {
         }
         executorService2.shutdown();
 
-
-        //  want to use interface with future result
+        // isTerminated example
         ExecutorService executorService3 = Executors.newSingleThreadExecutor();
-        Future<?>  future3 = executorService3.submit(() -> System.out.println("Hello"), "abc");
-        System.out.println(future3.get());
+        Future<Integer>  future3 = executorService3.submit(() -> 42);
         if(future3.isDone()){
-            System.out.println("Task is done ! ");
+            System.out.println("Task is done !");
         }
+        System.out.println(future3.get());
         executorService3.shutdown();
+        Thread.sleep(100);
+        System.out.println(executorService3.isTerminated());
+
+
+        //invokeAll
+        ExecutorService executorService4 = Executors.newFixedThreadPool(2);
+        Callable<Integer> callable1 = () -> {
+            System.out.println("task 1 ");
+            return 1;
+        };
+        Callable<Integer> callable2 = () ->{
+            System.out.println("task 2");
+            return 2;
+        };
+        Callable<Integer> callable3 = () -> {
+            System.out.println("task 3");
+            return 3;
+        };
+        List<Callable<Integer>> list = Arrays.asList(callable1, callable2, callable3);
+        List<Future<Integer>> futures = executorService4.invokeAll(list);
+        for (Future<Integer> future4 : futures){
+            System.out.println(future4.get());
+        }
+        executorService4.shutdown();
+
     }
 }
